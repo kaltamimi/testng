d66248dffb0f25629ad9e5f1345489e8421026d6
kawthar <kawthermujahed@gmail.com>
2019-03-23 17:58:19 +0200

    Reporting With Page Object Model refactor

--- src/main/java/extentreports/LoginTest.java
+++ src/main/java/extentreports/LoginTest.java
@@ -1,11 +1,8 @@
 package extentreports;
 
-import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
@@ -26,6 +23,7 @@ public class LoginTest {
 
     @BeforeClass
     public void beforeClass() {
+        System.setProperty("webdriver.gecko.driver", "./browser/geckodriver.exe");
         baseUrl = "http://www.letskodeit.com/";
         report = ExtentFactory.getInstance();
         test = report.startTest("Verify Welcome Text");
@@ -43,36 +41,15 @@ public class LoginTest {
 
     @Test
     public void test1_validLoginTest() throws Exception {
-        hp.clickSignUpLink();
-
-        WebElement loginLink = driver.findElement(By.id("signUpDialogswitchDialogLink"));
-        loginLink.click();
-        test.log(LogStatus.INFO, "Clicked on login link");
-
-        WebElement emailField = driver.findElement(By.xpath("//div[@id='memberLoginDialogemail']//input"));
-        emailField.sendKeys("test@email.com");
-        test.log(LogStatus.INFO, "Enter email");
-
-        WebElement passwordField = driver.findElement(By.xpath("//div[@id='memberLoginDialogpassword']//input"));
-        passwordField.sendKeys("abcabc");
-        test.log(LogStatus.INFO, "Enter password");
-
-        WebElement goButton = driver.findElement(By.id("memberLoginDialogsubmitButton"));
-        goButton.click();
-        test.log(LogStatus.INFO, "Clicked Go button");
+        hp.login("test@email.com", "abcabc");
 
         Thread.sleep(3000);
 
-        WebElement welcomeText = null;
+        boolean result = hp.isWelcomeTextPresent();
 
-        try {
-            welcomeText = driver.findElement(By.xpath("//div[text()='Hello test@email.com']"));
-        }
-        catch (NoSuchElementException e) {
-            System.out.println(e.getMessage());
-        }
-        Assert.assertTrue(welcomeText != null);
-        test.log(LogStatus.PASS, "Verified Welcome Text");	}
+        Assert.assertTrue(result);
+        test.log(LogStatus.PASS, "Verified Welcome Text");
+    }
 
     @AfterClass
     public void afterClass() {
